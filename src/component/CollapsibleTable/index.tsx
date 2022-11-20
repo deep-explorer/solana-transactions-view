5ae7adfc146ec13f28c59053ad9fe41f6e4e42d9
deep-explorer <daniel.lee@tbo.clothing>
2022-11-21 04:39:56 +0800

    fix to native table

--- src/component/CollapsibleTable/index.tsx
+++ src/component/CollapsibleTable/index.tsx
@@ -1,17 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-import Box from "@mui/material/Box";
-import Collapse from "@mui/material/Collapse";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import Typography from "@mui/material/Typography";
-import Paper from "@mui/material/Paper";
-
 type Transaction = {
   id: string;
   block_id: string;
@@ -42,55 +31,41 @@ function Row({ transaction }: { transaction: Transaction }) {
 
   return (
     <React.Fragment>
-      <TableRow
-        onClick={() => setOpen(!open)}
-        sx={{ "& > *": { borderBottom: "unset" } }}>
-        <TableCell>{transaction.id}</TableCell>
-        <TableCell>{transaction.block_number}</TableCell>
-        <TableCell>
+      <tr onClick={() => setOpen(!open)}>
+        <td>{transaction.id}</td>
+        <td>{transaction.block_number}</td>
+        <td>
           {duration ? duration + " minutes ago" : "less than 1 minute ago"}
-        </TableCell>
-        <TableCell>
+        </td>
+        <td>
           {transaction.events[0].amount /
             Math.pow(10, transaction.events[0].decimals)}
-        </TableCell>
-      </TableRow>
-
-      <TableRow>
-        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
-          <Collapse in={open} timeout="auto" unmountOnExit>
-            <Box sx={{ margin: 1 }}>
-              <Typography variant="h6" gutterBottom component="div">
-                Details
-              </Typography>
-              <Table size="small" aria-label="purchases">
-                <TableHead>
-                  <TableRow>
-                    <TableCell>From</TableCell>
-                    <TableCell>To</TableCell>
-                    <TableCell>Action Type</TableCell>
-                    <TableCell align="right">Amount</TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {transaction.events.map((detail) => (
-                    <TableRow key={detail.id}>
-                      <TableCell component="th" scope="row">
-                        {detail.source}
-                      </TableCell>
-                      <TableCell>{detail.destination}</TableCell>
-                      <TableCell>{detail.type}</TableCell>
-                      <TableCell align="right">
-                        {detail.amount / Math.pow(10, detail.decimals)}
-                      </TableCell>
-                    </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </Box>
-          </Collapse>
-        </TableCell>
-      </TableRow>
+        </td>
+      </tr>
+      <tr>
+        <td colSpan={3}>
+          <table>
+            <thead>
+              <tr>
+                <th>From</th>
+                <th>To</th>
+                <th>Action Type</th>
+                <th>Amount</th>
+              </tr>
+            </thead>
+            <tbody>
+              {transaction.events.map((detail) => (
+                <tr key={detail.id}>
+                  <td> {detail.source}</td>
+                  <td>{detail.destination}</td>
+                  <td>{detail.type}</td>
+                  <td>{detail.amount / Math.pow(10, detail.decimals)}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </td>
+      </tr>
     </React.Fragment>
   );
 }
@@ -98,40 +73,35 @@ function Row({ transaction }: { transaction: Transaction }) {
 export const CollapsibleTable = () => {
   const [transactions, setTransactions] = useState<Transaction[]>([]);
 
-  useEffect(() => {
-    axios
-      .get(
-        "https://svc.blockdaemon.com/universal/v1/solana/mainnet/account/rFqFJ9g7TGBD8Ed7TPDnvGKZ5pWLPDyxLcvcH2eRCtt/txs?page_size=100",
-        {
-          headers: {
-            Authorization:
-              "Bearer hkdjxWcQOMECMYN5vU8qY9hm6ynCQMPVp0hdRGYgAAcFZlv7",
-          },
-        }
-      )
-      .then((res) => {
-        console.log(res.data);
-        setTransactions(res.data.data);
-      });
-  }, []);
+  useEffect(() => {}, []);
 
+  const fetch = async () => {
+    const response = await axios.get(
+      "https://svc.blockdaemon.com/universal/v1/solana/mainnet/account/rFqFJ9g7TGBD8Ed7TPDnvGKZ5pWLPDyxLcvcH2eRCtt/txs?page_size=100",
+      {
+        headers: {
+          Authorization:
+            "Bearer hkdjxWcQOMECMYN5vU8qY9hm6ynCQMPVp0hdRGYgAAcFZlv7",
+        },
+      }
+    );
+    console.log(response);
+  };
   return (
-    <TableContainer component={Paper}>
-      <Table aria-label="collapsible table">
-        <TableHead>
-          <TableRow>
-            <TableCell className="w-1/2">Signature</TableCell>
-            <TableCell className="w-1/6">Block</TableCell>
-            <TableCell className="w-1/6">Time</TableCell>
-            <TableCell className="w-1/6">Fee(SOL)</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {transactions.map((transaction) => (
-            <Row key={transaction.id} transaction={transaction} />
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <table>
+      <thead>
+        <tr>
+          <th className="w-1/2">Signature</th>
+          <th className="w-1/6">Block</th>
+          <th className="w-1/6">Time</th>
+          <th className="w-1/6">Fee (SOL)</th>
+        </tr>
+      </thead>
+      <tbody>
+        {transactions.map((transaction) => (
+          <Row key={transaction.id} transaction={transaction} />
+        ))}
+      </tbody>
+    </table>
   );
 };
